c464eb745f31b28e1f87b826c76f691b325ea915
nasywa3032 <nasywa3032@gmail.com>
2024-02-02 17:04:52 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae, asperiores officia omnis porro quisquam, molestiae doloribus molestias tempora odit, architecto delectus earum reprehenderit consectetur harum. Totam eaque et dicta architecto.</p>
 </body>
 </html>
